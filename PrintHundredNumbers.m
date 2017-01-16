398c739aa97e9c49024a28a73497e8e9694be37e
araksify <araks.kpt@gmail.com>
2017-01-16 23:51:48 +0400

    Add files via upload

---
+++ PrintHundredNumbers.m
@@ -0,0 +1,21 @@
+//
+//  PrintHundredNumbers.m
+//  Kochaning
+//
+//  Created by Araks on 1/16/17.
+//  Copyright © 2017 Araks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        
+        for (int i = 1; i <= 100; i++) {
+            NSLog(@" Number is %i", i);
+        }
+        
+    }
+    return 0;
+}
