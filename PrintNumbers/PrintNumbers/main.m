def4b24258be891e4501e7e2c90fbaab6b26b64d
araksify <araks.kpt@gmail.com>
2017-01-18 00:38:34 +0400

    Add printing numbers from 1 to 100

---
+++ PrintNumbers/PrintNumbers/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  PrintNumbers
+//
+//  Created by Araks on 1/18/17.
+//  Copyright © 2017 Araks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        for (int i = 1; i <= 100; i++) {
+            NSLog(@"Number is %i", i);
+        }
+    }
+    return 0;
+}
