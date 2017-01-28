bdcf42cbb0a4ff4db74034f10924b2efac67f42a
araksify <araks.kpt@gmail.com>
2017-01-29 01:49:27 +0400

    Chapter 6 ex. 6 Hardwork is Done

--- Chapter6/Chapter6/main.m
+++ Chapter6/Chapter6/main.m
@@ -37,7 +37,7 @@ int main(int argc, char * argv[]) {
             }
             NSLog(@"\nAccumulator is %lf", [calculate accumulator]);
         }
-    //Excercise 6.4. Reverse the digits of an integer typed in from terminal. Consider that it might be negative number, so - sign will be logged at the end.
+    //Excercise 6.5. Reverse the digits of an integer typed in from terminal. Consider that it might be negative number, so - sign will be logged at the end.
         int typedNumber, rightDigit;
         
         NSLog(@"Please type a valid integer number you want to reverse.");
@@ -51,6 +51,60 @@ int main(int argc, char * argv[]) {
             typedNumber /= 10;
         }
         NSLog(@"-");
+    //Write a program that takes an integer keyed in from the terminal and extracts and displays each digit of the integer in English. (Remember to display zero if the user types in just 0.)
+        int typenDigit, leftDigit, power;
+        NSString *numEnglish;
+        NSLog(@"Please type a valid integer number to convert it in English.");
+        scanf("%i", &typenDigit);
+        
+        NSString *numToString;
+        numToString = [NSString stringWithFormat:@"%d", typenDigit];
+        int len = (int)[numToString length];
+        
+        do {
+            power = pow(10, (len - 1)); // asenq 4000i depqum 10-i 3, klini 1000
+            leftDigit = typenDigit / power; // 4000 / 1000 = 4
+            switch (leftDigit) {
+                case 1:
+                    numEnglish = (@"one");
+                    break;
+                case 2:
+                    numEnglish = (@"two");
+                    break;
+                case 3:
+                    numEnglish = (@"three");
+                    break;
+                case 4:
+                    numEnglish = (@"four");
+                    break;
+                case 5:
+                    numEnglish = (@"five");
+                    break;
+                case 6:
+                    numEnglish = (@"six");
+                    break;
+                case 7:
+                    numEnglish = (@"seven");
+                    break;
+                case 8:
+                    numEnglish = (@"eight");
+                    break;
+                case 9:
+                    numEnglish = (@"nine");
+                    break;
+                case 0:
+                    numEnglish = (@"zero");
+                    break;
+                default:
+                    numEnglish = (@"oye this is wrong input!");
+                    break;
+            }
+            typenDigit = typenDigit - leftDigit * power;
+            NSLog(@"%@", numEnglish);
+            len--;
+        } while (len != 0);
+        
+
     }
     return 0;
 }
